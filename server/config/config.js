e79dea6f3b7ae3b33615cc537cc13b757ff06abf
fabian de la rosa <fabdel_0123@outlook.com>
2021-01-24 19:03:33 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/EstudiantesCUR';
 
 } else {
-    urlDB = 'mongodb+srv://adminAtlasCUR:Andresfabian782@cluster0.x5nqo.mongodb.net/EstudiantesCUR';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
