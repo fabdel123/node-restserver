0ee5f17c017b278fcd45e5a01a191ebbcba1e8eb
fabian de la rosa <fabdel_0123@outlook.com>
2021-02-08 23:45:03 -0500

    Login-N

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
-
-
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -15,13 +13,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Configurar CORS (Para que permita hacer peticiones tanto entrantes como salientes)
-app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-	next();
-});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://rest-server-cur782.herokuapp.com"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 //Configuración Global de Rutas
 app.use( require('./routes/index') );
